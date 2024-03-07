13cc297d9839a9216dfa88efd96185244f8cf4d8
Natansh14687 <natansh522@gmail.com>
2024-03-07 16:11:36 +0530

    Following this journey to data science

---
+++ MySQL/Starting-with-MySQL.sql
@@ -0,0 +1,10 @@
+create database pw;
+use pw;
+select * from denver;
+select storenumber from denver;
+select name,phonenumber from denver;
+select name as name__,phonenumber as phone_number from denver;
+select distinct storenumber from denver;
+select distinct postalcode as postal_code from denver;
+
+
