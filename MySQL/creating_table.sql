b6e8c814cdec5d3e4b73e20faa03a3927e6fc912
Natansh14687 <natansh522@gmail.com>
2024-03-09 13:03:47 +0530

    creating table in mysql

---
+++ MySQL/creating_table.sql
@@ -0,0 +1,17 @@
+show databases;
+use dbpanda;
+show tables;
+create table test(id int, name varchar(100));
+select * from test;
+insert into test values(1,"Natansh");
+insert into test values(2,"Dushyant");
+insert into test values(3,"Aman");
+insert into test values(4,"Rohit");
+insert into test values(5,"Hemant");
+insert into test values(6,"Navneet");
+insert into test values(7,"Subhasis");
+
+insert into test values(8,"Prakhar"),(9,"Yogi"),(10,"Aryan");
+
+
+
